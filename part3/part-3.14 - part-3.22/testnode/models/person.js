54063218f07b5333e2ebbc7aa75fee4c37451e00
Rishitha Duggempudi <73739978+student-02-a074c1c8eb6c@users.noreply.github.com>
2021-06-12 11:19:08 +0530

    Create person.js

---
+++ part3/part-3.14 - part-3.22/testnode/models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const url = `mongodb+srv://fullstack:Password@cluster0.ryz57.mongodb.net/Phonebook?retryWrites=true&w=majority`
+
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+  important: Boolean,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
