618e274cba50aff175da046fc3d79e50fc83c441
Rishitha Duggempudi <73739978+student-02-a074c1c8eb6c@users.noreply.github.com>
2021-06-12 09:55:05 +0530

    Create person.js

---
+++ part3/part-3.13/frontend/src/services/person.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const baseUrl = '/personss'
+
+const getAll = async () => {
+    const request = axios.get(baseUrl)
+    return request.then(request => request.data)
+}
+
+const create = async (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(request => request.data)
+    
+}
+
+const update = async (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(request => request.data)
+}
+
+const deletePerson = async (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(request => request.data)
+}
+
+export default {
+    getAll,
+    create,
+    update,
+    deletePerson
+}
