17a9e3b479eccf4fa43325de2723e9789037d26c
Rishitha Duggempudi <73739978+student-02-a074c1c8eb6c@users.noreply.github.com>
2021-06-14 09:47:48 +0530

    Update index.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,1 +1,11 @@
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
+
+
+server.listen(process.env.PORT, () => {
+  logger.info(`Server running on port ${process.env.PORT}`)
+})
